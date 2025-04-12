5fb6db0292438654e6f6a4f89cfae54f4a1895d9
AbdulRehman <153705975+AbdulRehman817@users.noreply.github.com>
2025-04-12 11:42:26 +0500

    Update Home.jsx

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -12,9 +12,7 @@ const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="bg-[#f7fbfc] w-full overflow-hidden">
-      {/* Wrapper for Consistent Layout */}
-      <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 space-y-12">
+    
         <section>
           <Hero />
         </section>
@@ -42,8 +40,7 @@ const Home = () => {
         <section>
           <NewsletterSection />
         </section>
-      </div>
-    </div>
+      
   );
 };
 
