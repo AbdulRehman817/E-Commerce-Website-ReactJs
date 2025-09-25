7d0b1c8c991036ec15a4b457f04453ad2961e69d
Abdul Rehman <abdulrehmanbey1718@gmail.com>
2025-09-25 13:45:45 +0500

    added / at register in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -39,7 +39,7 @@ const router = createBrowserRouter([
     ],
   },
   { path: "/Login", element: <Login /> },
-  { path: "Register", element: <Register /> },
+  { path: "/Register", element: <Register /> },
 ]);
 
 // Render Application
